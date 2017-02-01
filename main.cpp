f331b50278d614b24748b1e0695c34ffb4ff7527
VictoriaMartinez <moyuku98@gmail.com>
2017-02-01 07:52:54 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,15 +4,24 @@
 using namespace std;
 
 int x, y, z;
+int suma, resta;
 
 multiplicacion();
+void suma_resta();
 
 int main()
 {
  
   
-  
+  suma_resta();
   cout << endl << endl;
   system("PAUSE");
   return 0;
+}
+
+void suma_resta()
+{
+ suma = x + y;
+ resta = x - y;
+ return;
 }
